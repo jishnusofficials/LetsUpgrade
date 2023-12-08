18199d19b734b8afcb156affc8af3432c3f1006a
Jishnu S <jishnu.innovation@gmail.com>
2023-12-08 18:54:09 +0530

    React Zero to Hero Day 3

--- src/App.js
+++ src/App.js
@@ -4,17 +4,13 @@ import Header from './components/Header';
 import Products from './components/ProductsList';
 
 function App() {
-  function doSomething(data)
-  {
-    console.log("Hello! I am Clicked"+data);
-  }
   return (
     <div className="App">
       <h1>Welcome Everyone</h1>
       <Header></Header>
       <Products></Products>
       {/* <button className='btn' onClick={doSomething}>Click</button> */}
-      <button className='btn' onClick={()=>{doSomething("Jishnu")}}>Click</button>
+     {/*  <button className='btn' onClick={()=>{doSomething("Jishnu")}}>Click</button> */}
     </div>
   );
 }
--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -1,11 +1,20 @@
 import Product from "./Product";
 function ProductsList ()
 {
+    let products =[
+    {id:1,name:"Levi Jeans",price:1500,url:"https://storage.sg.content-cdn.io/cdn-cgi/image/width=1034,height=1376,quality=75,format=auto,fit=cover,g=top/in-resources/6ff2919b-8254-4153-83d9-376e98b3c072/Images/ProductImages/Source/Levis-Mens-Made-in-Japan-1980s-501-Jeans-A58750002_01_Front.jpg"},
+    {id:2,name:"Samsung M53", price:25000,url:"https://i.gadgets360cdn.com/products/large/samsung-galaxy-m53-647x800-1650604669.jpg?downsize=*:180"},
+    {id:3,name:"Dell Monitor", price:10000,url:"https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/peripherals/monitors/u-series/u2723qe/media-gallery/monitor-u2723qe-gallery-1.psd?fmt=png-alpha&pscan=auto&scl=1&hei=476&wid=797&qlt=100,1&resMode=sharp2&size=797,476&chrss=full"}
+    ]
     return(
         <div className='products'>
-            <Product name="Levi Jeans" price="1500" url="https://storage.sg.content-cdn.io/cdn-cgi/image/width=1034,height=1376,quality=75,format=auto,fit=cover,g=top/in-resources/6ff2919b-8254-4153-83d9-376e98b3c072/Images/ProductImages/Source/Levis-Mens-Made-in-Japan-1980s-501-Jeans-A58750002_01_Front.jpg"/>
-            <Product name="Samsung M53" price="25000" url="https://i.gadgets360cdn.com/products/large/samsung-galaxy-m53-647x800-1650604669.jpg?downsize=*:180"/>
-            <Product name="Dell Monitor" price="10000" url="https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/peripherals/monitors/u-series/u2723qe/media-gallery/monitor-u2723qe-gallery-1.psd?fmt=png-alpha&pscan=auto&scl=1&hei=476&wid=797&qlt=100,1&resMode=sharp2&size=797,476&chrss=full"/>
+            {
+                products.map((product)=>{
+                    return (
+                        <Product name={product.name} price={product.price} url={product.url} key={product.id}/>
+                    )
+                })
+            }
 
         </div>
     )
